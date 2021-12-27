5cc6ca572e14fa177265dfc6520340c81ca4363a
Hoanle396 <89791546+Hoanle396@users.noreply.github.com>
2021-12-27 12:54:51 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,11 @@ Route::get('/product/{id}','ApiProductController@show');
 Route::get('getbank','CheckoutController@getBank');
 Route::post('checkout','CheckoutController@store');
 Route::post('done','CheckoutController@done');
-
-
-Route::resource('product','ApiProductController');
+Route::get('search/{search}','ApiProductController@search');
+Route::get('fillter/{value}','ApiProductController@filters');
+//Route::resource('product','ApiProductController');
 Route::resource('feedback','Admin\FeedbackController');
 Route::post('/service' ,'Admin\ServiceController@store');
 Route::get('news','NewsController@index');
 Route::get('news/{id}','NewsController@show');
+Route::get('history/{email}','ApiUserController@show');
